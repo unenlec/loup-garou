3a853787298c8d383111d3a138a4bd6843ec5994
unenlec <unenlec>
2024-02-19 10:56:48 +0100

    ajout de react-router + pages + tailwind

---
+++ client/src/pages/error-page.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom"
+
+export default function ErrorPage(){
+    return(
+        <div>
+            Error 404 Not Found OOPS
+            <Link to="/">Accueil</Link>
+        </div>
+    )
+}
---
+++ client/src/pages/register.jsx
@@ -0,0 +1,13 @@
+export default function Register()
+{
+    return(
+        <div className="flex flex-col gap-3">
+            <h1>Inscription</h1>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="password"/>
+            <input type="password"/>
+            <button>Valider</button>
+        </div>
+    )
+}
