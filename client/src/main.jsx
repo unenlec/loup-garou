48ccc16f5387aeb3a0c8709dfb0b56f431fe2f80
unenlec <unenlec>
2024-02-19 10:56:48 +0100

    ajout de react-router + pages + tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter,RouterProvider} from  'react-router-dom';
+import ErrorPage from './pages/error-page.jsx';
+import Login from './pages/login.jsx';
+import Register from './pages/register.jsx';
+import Game from './pages/game.jsx';
 
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path:"/login",
+    element: <Login/>
+  },
+  {
+    path:"/register",
+    element: <Register/>
+  },
+  {
+    path:"/game",
+    element:<Game/>
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,11 @@
+export default function Login()
+{
+    return(
+        <div className="flex flex-col gap-3">
+            <h1>Connexion</h1>
+            <input type="text"/>
+            <input type="password"/>
+            <button>Valider</button>
+        </div>
+    )
+}
