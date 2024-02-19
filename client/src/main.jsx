2b8084868a9d1786b79c6536291ae9d739fe65df
Arth-y <159006848+Arth-y@users.noreply.github.com>
2024-02-19 11:44:08 +0100

    Ajout de la premiere home page avec login.jsx et son login.css pour les styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import LOGIN from './components/login.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LOGIN />
   </React.StrictMode>,
 )
