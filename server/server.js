799a98f784df369a6f260bb5527b71e4c1e8b2a9
unenlec <unenlec>
2024-02-20 12:30:44 +0100

    test chat

--- server/server.js
+++ server/server.js
@@ -4,6 +4,32 @@ const port = 4000
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes.js");
 const connectToMongoDB = require("./database/connectToMongoDB.js");
+const cors = require("cors");
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+app.use(cors());
+const io = new Server(server,{
+    cors:{
+        origin:'*'
+    }
+});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on("message",(data)=>{
+    console.log(data)
+    socket.broadcast.emit("receive",data)
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
+
+/*
 dotenv.config();
 app.use(express.json());
 
@@ -12,4 +38,4 @@ app.use("/api/auth", authRoutes.router);
 app.listen(port, () => {
     connectToMongoDB.connectToMongoDB();
     console.log(`Listening on port ${port}`)
-})
+})*/
