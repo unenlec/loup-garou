fc0a3e12e4dca67e8746e4a276285c7853375826
Arth-y <159006848+Arth-y@users.noreply.github.com>
2024-03-04 13:18:57 +0100

    Debut game.jsx

--- client/src/pages/game.jsx
+++ client/src/pages/game.jsx
@@ -15,16 +15,47 @@ export default function Game()
         })
     },[socket])
     return(
-        <div>
-            <h1>GAME</h1>
-            <div className="flex flex-col">
-                {messageReceived.map((m)=>(
-                    <p>{m}</p>
-                ))}
+        <div className="flexbox">
+            <div className = "w-3/4 bg-red-400 absolute inset-y-0 left-0 ">
+                
+                <div className="fixed top-0 right-2/3">
+                    Joueur 1
+                    <div className="flexbox bg-blue-400 w-20 h-20"></div>
+                </div>
+
+                <div className="fixed top-1/4">
+                    Joueur 2
+                    <div className="flexbox bg-blue-400 w-20 h-20"></div>
+                </div>
+
+                <div className="fixed  bottom-1/4  right-2/3">
+                    Joueur 3
+                    <div className="flexbox bg-blue-400 w-20 h-20"></div>
+                </div>
+
+                <div className="fixed top-2/4 left-2/4">
+                    Joueur 4
+                    <div className="flexbox bg-blue-400 w-20 h-20"></div>
+                </div>
+
+                <div className="relative  top-2/4 left-3/4">
+                    Joueur 5
+                    <div className="flexbox bg-blue-400 w-20 h-20"></div>
+                </div>
+
+
             </div>
-            <input onChange={(event)=>setMessage(event.target.value)}placeholder="Message"/>
-            <button onClick={sendMessage}>Envoyer</button>
-            
+
+            <div className = "w-1/4 bg-lime-500 absolute inset-y-0 right-0">
+            <div className = "absolute bottom-0 right-0 w-full">
+            <div className="flex flex-col bg-blue-500">
+                    {messageReceived.map((m)=>(
+                        <p>{m}</p>
+                    ))}
+                </div>
+                <input className = "w-full" onChange={(event)=>setMessage(event.target.value)}placeholder="Message"/>
+                <button className = "w-full" onClick={sendMessage}>Envoyer</button>
+            </div></div>
         </div>
     )
 }
