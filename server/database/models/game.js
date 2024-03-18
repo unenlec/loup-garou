a5152fe292c41ff84c621acb93c77c9eb5b90075
Arth-y <159006848+Arth-y@users.noreply.github.com>
2024-03-18 10:43:45 +0100

    merge commit

---
+++ server/database/models/game.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+
+    uuid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    owner: {
+        type: mongoose.ObjectId,
+        required: true,
+    },
+    players: {
+        type: [mongoose.ObjectId]
+    },
+    slot: {
+        type: Number,
+        default: 4
+    },
+    finished:{
+        type:Boolean,
+        default: false
+    }
+});
+
+const Game = mongoose.model("Game", gameSchema);
+module.exports = Game;
